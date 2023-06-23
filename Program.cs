8dfa700e86d39fe14a30c33682a930462167f96e
HANAMICRO\sorachotj <sorachotj@lpn.hanabk.th.com>
2023-06-23 09:37:24 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+//make function aad two number if result is greater than 10 then subtract 5 from result
+//if result is less than 10 then add 5 to result
+
+int AddTwoNumbers(int num1, int num2)
+{
+    int result = num1 + num2;
+    if (result > 10)
+    {
+        result = result - 5;
+    }
+    else
+    {
+        result = result + 5;
+    }
+    return result;
+}
+
+//create unit tests for function
+
+Console.WriteLine(AddTwoNumbers(2, 3) == 5);
+Console.WriteLine(AddTwoNumbers(10, 10) == 15);
+Console.WriteLine(AddTwoNumbers(5, 5) == 10);
+Console.WriteLine(AddTwoNumbers(5, 6) == 6);
+Console.WriteLine(AddTwoNumbers(1, 2) == 3);
+Console.WriteLine(AddTwoNumbers(1, 2) == 4);
+Console.WriteLine(AddTwoNumbers(1, 2) == 5);
+Console.WriteLine(AddTwoNumbers(1, 2) == 6);
+Console.WriteLine(AddTwoNumbers(1, 2) == 7);
+Console.WriteLine(AddTwoNumbers(1, 2) == 8);
+Console.WriteLine(AddTwoNumbers(1, 2) == 9);
+Console.WriteLine(AddTwoNumbers(1, 2) == 10);
+Console.WriteLine(AddTwoNumbers(1, 2) == 11);
+Console.WriteLine(AddTwoNumbers(1, 2) == 12);
+Console.WriteLine(AddTwoNumbers(1, 2) == 13);
+Console.WriteLine(AddTwoNumbers(1, 2) == 14);
+Console.WriteLine(AddTwoNumbers(1, 2) == 15);
+Console.WriteLine(AddTwoNumbers(1, 2) == 16);
+Console.WriteLine(AddTwoNumbers(1, 2) == 17);
+Console.WriteLine(AddTwoNumbers(1, 2) == 18);
+Console.WriteLine(AddTwoNumbers(1, 2) == 19);
+Console.WriteLine(AddTwoNumbers(1, 2) == 20);
+Console.WriteLine(AddTwoNumbers(1, 2) == 21);
+Console.WriteLine(AddTwoNumbers(1, 2) == 22);
+Console.WriteLine(AddTwoNumbers(1, 2) == 23);
+Console.WriteLine(AddTwoNumbers(1, 2) == 24);
+Console.WriteLine(AddTwoNumbers(1, 2) == 25);
+Console.WriteLine(AddTwoNumbers(1, 2) == 26);
+Console.WriteLine(AddTwoNumbers(1, 2) == 27);
+Console.WriteLine(AddTwoNumbers(1, 2) == 28);
+Console.WriteLine(AddTwoNumbers(1, 2) == 29);
+ 
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
